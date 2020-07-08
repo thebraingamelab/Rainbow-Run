ad93d4994cf148996f549d1526f581af215047e2
LingQi000809 <lqi@vassar.edu>
2020-07-08 13:12:43 -0400

    Update map.js

--- code/map.js
+++ code/map.js
@@ -34,10 +34,10 @@ function colorShape() {
         // notes
         let colorNotes = [];
         for (let i = 0; i < nTiles; i++) {
-            let curNote = notes[Math.random(notes.length)];
+            let curNote = notes[Math.floor(Math.random(notes.length))];
             let sharp = '';
-            if (Math.random(2) === 0) sharp = 's';
-            let range = Math.random(3) + 3; //piano range: [3,4,5]
+            if (Math.floor(Math.random(2) === 0)) sharp = 's';
+            let range = Math.floor(Math.random(3)) + 3; //piano range: [3,4,5]
             let curPitch = curNote + sharp + range;
             let repeated = false;
             for (let j=0; j<i; j++){
