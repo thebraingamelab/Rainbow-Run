d5c78c07250ed2c9a11ad3e16f5a6e6115fd4eb8
LingQi000809 <lqi@vassar.edu>
2020-07-10 11:50:11 -0400

    Update variables.js

--- code/variables.js
+++ code/variables.js
@@ -181,84 +181,84 @@ function startGame() {
     errorAudio = new Sound("audio/error.mp3", context, _sfxGainNode);
     fallAudio = new Sound("audio/whoosh.mp3", context, _sfxGainNode);
     gameOverAudio = new Sound("audio/gameOver.mp3", context, _sfxGainNode);
+}
 
-    function setTileParaByWidth(tileWidth) {
-        tileLength = tileWidth / 1.5;
-        tileHeight = tileLength / 5;
-        xDistance = tileWidth / 2 + tileHeight * 1.5; // distance from the last tile on x-axis
-        yDistance = tileHeight + tileLength / 2; // distance from the last tile on y-axis
+function setTileParaByWidth(tileWidth) {
+    tileLength = tileWidth / 1.5;
+    tileHeight = tileLength / 5;
+    xDistance = tileWidth / 2 + tileHeight * 1.5; // distance from the last tile on x-axis
+    yDistance = tileHeight + tileLength / 2; // distance from the last tile on y-axis
 
+}
+
+function setTileParaByLength(tileLength) {
+    tileWidth = tileLength * 1.5;
+    tileHeight = tileLength / 5;
+    xDistance = tileWidth / 2 + tileHeight * 1.5; // distance from the last tile on x-axis
+    yDistance = tileHeight + tileLength / 2; // distance from the last tile on y-axis
+}
+
+
+// Sound object
+function Sound(filePath, audioContext, gainNode, loop = false) {
+    let my = this;
+    // let testAudio;
+    let xhr;
+
+    // Initialize fields (constructor stuff)
+    this.buffer = null;
+    this.audioContext = audioContext;
+    this.gainNode = gainNode;
+    this.loop = loop;
+
+    // Check for file type compatibility
+    testAudio = document.createElement("audio");
+
+    // Fetch the file
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', encodeURI(filePath), true);
+    xhr.responseType = 'arraybuffer';
+    // xhr.onload = function() {
+    //     context.decodeAudioData(xhr.response, function(buffer) {
+    //       my.buffer = buffer;
+    //     }, onError);
+    //   }
+    // Oopsie doopsie, couldn't fetch the file
+    xhr.addEventListener("error", function () {
+        console.log('Error loading from server: ' + filePath);
+    }, false);
+    // On successful load, decode the audio data
+    xhr.addEventListener("load", function () {
+        audioContext.decodeAudioData(xhr.response,
+            // Success
+            function (audioBuffer) {
+                my.buffer = audioBuffer;
+            },
+            // Error
+            function (e) {
+                console.log("Error decoding audio data: " + e.err);
+            });
+    }, false);
+    xhr.send();
+}
+
+// Play function, for playing the sound
+Sound.prototype.play = function () {
+    let thisObject = this;
+
+    // Play the sound only if it's been decoded already
+    if (this.buffer) {
+        let bufferSource = this.audioContext.createBufferSource();
+        bufferSource.buffer = this.buffer;
+        bufferSource.connect(this.gainNode).connect(this.audioContext.destination);
+        bufferSource.start(0);
+        bufferSource.loop = this.loop;
     }
 
-    function setTileParaByLength(tileLength) {
-        tileWidth = tileLength * 1.5;
-        tileHeight = tileLength / 5;
-        xDistance = tileWidth / 2 + tileHeight * 1.5; // distance from the last tile on x-axis
-        yDistance = tileHeight + tileLength / 2; // distance from the last tile on y-axis
-    }
-
-
-    // Sound object
-    function Sound(filePath, audioContext, gainNode, loop = false) {
-        let my = this;
-        // let testAudio;
-        let xhr;
-
-        // Initialize fields (constructor stuff)
-        this.buffer = null;
-        this.audioContext = audioContext;
-        this.gainNode = gainNode;
-        this.loop = loop;
-
-        // Check for file type compatibility
-        testAudio = document.createElement("audio");
-
-        // Fetch the file
-        xhr = new XMLHttpRequest();
-        xhr.open('GET', encodeURI(filePath), true);
-        xhr.responseType = 'arraybuffer';
-        // xhr.onload = function() {
-        //     context.decodeAudioData(xhr.response, function(buffer) {
-        //       my.buffer = buffer;
-        //     }, onError);
-        //   }
-        // Oopsie doopsie, couldn't fetch the file
-        xhr.addEventListener("error", function () {
-            console.log('Error loading from server: ' + filePath);
-        }, false);
-        // On successful load, decode the audio data
-        xhr.addEventListener("load", function () {
-            audioContext.decodeAudioData(xhr.response,
-                // Success
-                function (audioBuffer) {
-                    my.buffer = audioBuffer;
-                },
-                // Error
-                function (e) {
-                    console.log("Error decoding audio data: " + e.err);
-                });
-        }, false);
-        xhr.send();
-    }
-
-    // Play function, for playing the sound
-    Sound.prototype.play = function () {
-        let thisObject = this;
-
-        // Play the sound only if it's been decoded already
-        if (this.buffer) {
-            let bufferSource = this.audioContext.createBufferSource();
-            bufferSource.buffer = this.buffer;
-            bufferSource.connect(this.gainNode).connect(this.audioContext.destination);
-            bufferSource.start(0);
-            bufferSource.loop = this.loop;
-        }
-
-        // If it hasn't been decoded yet, check every 50ms to see if it's ready
-        else {
-            window.setTimeout(function () {
-                thisObject.play();
-            }, 50);
-        }
+    // If it hasn't been decoded yet, check every 50ms to see if it's ready
+    else {
+        window.setTimeout(function () {
+            thisObject.play();
+        }, 50);
     }
 }
